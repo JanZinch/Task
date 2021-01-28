8f9b7b8fda85e9fbb03438f09ab070a69a238f63
JanZinch <jan.zinchanka@mail.ru>
2021-01-28 13:15:06 +0300

    New_commit

---
+++ Task/Assets/Scripts/LevelsManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+public class LevelsManager : MonoBehaviour
+{
+    private TextMeshProUGUI _levelNumberLabel;
+    [SerializeField] private TextMeshProUGUI _coinsCountLabel;
+
+    private short _requiredCoins;
+    private short _currentCoins;
+
+    [SerializeField] private Mission _displayedMission;
+    public Mission DisplayedMission {
+
+        get {
+
+            return _displayedMission;        
+        }
+
+        set
+        {
+            _displayedMission = value;
+            _levelNumberLabel.text = "Level: " + _displayedMission.NumberLabel.text;
+            _requiredCoins = _displayedMission.RequiredCoins;
+            _currentCoins = _displayedMission.CurrentCoins;
+            _coinsCountLabel.text =  "x" + _currentCoins + "/" + _requiredCoins;
+
+        }
+
+    }
+
+    public string LevelNumber {
+
+        get {
+
+            return _levelNumberLabel.text;
+        }
+
+        set {
+
+            _levelNumberLabel.text = "Level: " + value;
+        }  
+    }
+
+    public string CoinsData {
+
+        get
+        {
+            return _coinsCountLabel.text;
+        }
+
+        set
+        {
+            _coinsCountLabel.text = value;
+        }
+    }
+
+    public void AddCoin() {
+
+        if (_currentCoins == _requiredCoins) return; 
+
+        _currentCoins++;
+        _coinsCountLabel.text = "x" + _currentCoins + "/" + _requiredCoins;
+        _displayedMission.AddCoin();
+    }
+
+
+    public void RemoveCoin() {
+
+        if (0 == _currentCoins) return; 
+
+        _currentCoins--;
+        _coinsCountLabel.text = "x" + _currentCoins + "/" + _requiredCoins;
+        _displayedMission.RemoveCoin();
+
+    }
+
+    void Start()
+    {        
+        _levelNumberLabel = GetComponent<TextMeshProUGUI>();
+        DisplayedMission = _displayedMission;
+    }
+
+
+}
---
+++ Task/Assets/Scripts/Mission.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+public class Mission : MonoBehaviour
+{
+    private short _number;
+    [SerializeField] private short _requiredCoins;
+    [SerializeField] private short _currentCoins;
+
+    public short RequiredCoins {
+
+        get {
+
+            return _requiredCoins;
+        }
+
+        set {
+
+            _requiredCoins = value;        
+        }    
+    }
+
+    public short CurrentCoins
+    {
+        get
+        {
+            return _currentCoins;
+        }
+
+        set
+        {
+            _currentCoins = value;
+        }
+    }
+
+    [SerializeField] private LevelsManager _levelsManager;
+    [SerializeField] private TextMeshProUGUI _numberLabel;
+    [SerializeField] private TextMeshProUGUI _coinsCountLabel;
+
+    public TextMeshProUGUI NumberLabel {
+
+        private set {
+
+            _numberLabel = value;
+        }
+
+        get {
+
+            return _numberLabel; 
+        }    
+    }
+
+    public TextMeshProUGUI CoinsLabel {
+
+        private set {
+
+            _coinsCountLabel = value;        
+        }
+
+        get {
+
+            return _coinsCountLabel; 
+        }
+    
+    }
+
+    
+
+    //private bool _completed;
+
+    public void SetLevel()
+    {
+        //_levelsManager.LevelNumber = _numberLabel.text;
+        //_levelsManager.CoinsData = _coinsLabel.text;
+
+        _levelsManager.DisplayedMission = this.GetComponent<Mission>();
+
+    }
+
+
+    public void AddCoin() {
+
+        _currentCoins++;
+        _coinsCountLabel.text = "x" + _currentCoins + "/" + _requiredCoins;
+    }
+
+    public void RemoveCoin()
+    {
+        _currentCoins--;
+        _coinsCountLabel.text = "x" + _currentCoins + "/" + _requiredCoins;
+    }
+
+
+    void Start()
+    {
+        try {
+
+            _number = short.Parse(_numberLabel.text);
+
+            //Debug.Log("CN: " + _number);
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+
+        _coinsCountLabel.text = "x" + _currentCoins + "/" + _requiredCoins;  
+        
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
